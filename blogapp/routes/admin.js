015e1d80d27944cbe369582611e4ddc35c1f67d5
devIsaque03 <i.09102024@gmail.com>
2025-02-11 14:35:20 -0300

    validar formulario express

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -20,17 +20,40 @@ router.get('/categorias/add', (req, res) => {
     res.render("admin/addcategorias")
 })
 
+
 router.post('/categorias/nova', (req, res) => {
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "nome inválido"})
     }
 
-    new Categoria(novaCategoria).save().then(() => {
-        console.log("Categoria Cadastrada com Sucesso!")
-    }).catch((erro) => {
-        console.log(`Erro ao Cadastrar: ${erro}`)
-    })
+    
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "slug inválido"})
+    }
+
+    if (req.body.nome.length <2 ) {
+        erros.push({texto: "Nome da categoria é muito pequeno!"})
+    }
+
+    if(erros.length > 0){
+        res.render('admin/addcategorias', {erros: erros})
+    } else {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+    
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash('success_msg', "categoria criada com sucesso!")
+            res.redirect('/admin/categorias')
+        }).catch((erro) => {
+            req.flash('error_msg', "Houve um erro ao salvar a categoria, tente novamente!")
+            res.redirect('/admin')
+        })
+    }
 })
 
 module.exports = router
