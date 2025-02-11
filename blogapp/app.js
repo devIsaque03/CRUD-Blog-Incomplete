eac83d455a2b28f08250fb7fcaabe872bedf89c2
devIsaque03 <i.09102024@gmail.com>
2025-02-11 11:13:53 -0300

    grupo de rotas no express

--- blogapp/app.js
+++ blogapp/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const APP = express()
+const admin = require("./routes/admin")
 // const mongoose = require('mongoose')
 
 // Configurações -----------------------------------------------------
@@ -10,14 +11,19 @@ const APP = express()
         APP.use(bodyParser.urlencoded({extend: true}))
         APP.use(bodyParser.json())
     // handlebars
-        APP.engine('handlebars', handlebars({deffaultLayout: 'main'}))
-        APP.set('view engine', 'handlebars');
+        APP.engine('handlebars', handlebars.create({ defaultLayout: 'main' }).engine) // Correção aqui
+        APP.set('view engine', 'handlebars')
     // mongoose
         // Em Breve
     //
     
 // Rotas -------------------------------------------------------------
+    APP.use('/admin', admin)
 
+
+
+
+    
 // Outros ------------------------------------------------------------
 const PORT = 8080
 APP.listen(PORT, () => {
